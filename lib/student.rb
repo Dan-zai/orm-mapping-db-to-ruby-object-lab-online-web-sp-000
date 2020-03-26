cda1b2bed96a48bd9d41def4d8c2c52ebb70d5cb
Daniel Cowpertwait <danielcowpertwait@gmail.com>
2020-03-26 05:23:44 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -63,7 +63,7 @@ class Student
   
   def self.students_below_12th_grade
     sql = <<-SQL
-    SELECT * WHERE grade < 12
+    SELECT * students WHERE grade < 12
     SQL
     DB[:conn].execute(sql)
   end
